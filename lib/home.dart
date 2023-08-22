31de33313120c7f2c47e8c9d0857c1a867464c89
BLUECALF <89413138+BLUECALF@users.noreply.github.com>
2023-08-23 01:07:09 +0300

    ADD : added back logic and press back to again to exit

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -9,33 +11,59 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final String targetUrl = 'https://dev.bideyako.com/';
   bool isLoading = true;
+  final _webViewController = Completer<WebViewController>();
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  DateTime? lastBackPressed;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: SafeArea(
-        child: Stack(
-          children: [
-            WebView(
-              initialUrl: targetUrl,
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageStarted: (url) {
-                setState(() {
-                  isLoading = true;
-                });
-              },
-              onPageFinished: (url) {
-                setState(() {
-                  isLoading = false;
-                });
-              },
-            ),
-            isLoading
-                ? Center(
-              child: CircularProgressIndicator(),
-            )
-                : Container(), // This container will be empty when the WebView finishes loading
-          ],
+        child: WillPopScope(
+          onWillPop: () async {
+            final controller = await _webViewController.future;
+            if (await controller.canGoBack()) {
+              controller.goBack();
+              return Future.value(false);
+            }
+
+            final now = DateTime.now();
+            if (lastBackPressed == null || now.difference(lastBackPressed!) > Duration(seconds: 2)) {
+              lastBackPressed = now;
+              final snackBar = SnackBar(content: Text('Press back again to exit'));
+              _scaffoldKey.currentState!.showSnackBar(snackBar);
+              return Future.value(false);
+            }
+            return Future.value(true);
+          },
+          child: Stack(
+            children: [
+              WebView(
+                initialUrl: targetUrl,
+                javascriptMode: JavascriptMode.unrestricted,
+                onPageStarted: (url) {
+                  setState(() {
+                    isLoading = true;
+                  });
+                },
+                onPageFinished: (url) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+                onWebViewCreated: (controller) {
+                  _webViewController.complete(controller);
+                },
+              ),
+              isLoading
+                  ? Center(
+                child: CircularProgressIndicator(),
+              )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
