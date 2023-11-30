d1b5508292641c8d294ba948795579582c75fa3c
BLUECALF <89413138+BLUECALF@users.noreply.github.com>
2023-11-30 22:58:21 +0300

    ADD : a delay in main to abid white screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:bide_yako/loading.dart';
 import 'package:bide_yako/utils/HelpfulFunctions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(milliseconds: 300));
+  // set screen orinetation to potrait
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
